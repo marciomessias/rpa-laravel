e70ff1f49b9588dd931a847c254eae653036f25e
Marcio Messias <marciomessias.developer@gmail.com>
2023-01-31 12:46:12 -0300

    criado rpa para baixar e renomear arquivo

--- app/Http/Controllers/RPA/DownloadAndSaveFileController.php
+++ app/Http/Controllers/RPA/DownloadAndSaveFileController.php
@@ -8,17 +8,20 @@ use Illuminate\Support\Facades\Storage;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\WebDriverBy;
-// use Facebook\WebDriver\Chrome\ChromeOptions;
 
 class DownloadAndSaveFileController extends Controller
 {
+    private $seleniumPath;
+    private $downloadedPath;
+
+    public function __construction()
+    {
+        $this->seleniumPath = Storage::path('selenium/');
+        $this->downloadedPath = Storage::path('downloaded/');
+    }
+
     public function init()
     {
-        // $options = new ChromeOptions();
-        // $prefs = array('download.default_directory' => env('DOWNLOAD_SELENIUM'));
-        // $options->setExperimentalOption('prefs', $prefs);
-        // $capabilities = DesiredCapabilities::chrome();
-        // $capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
         $driver = RemoteWebDriver::create(env('SERVER_SELENIUM'), DesiredCapabilities::chrome());
 
         try {
@@ -29,9 +32,11 @@ class DownloadAndSaveFileController extends Controller
 
             $directDownloadId = $driver->findElement(WebDriverBy::id('direct-download'))->click();
 
+            sleep(3);
 
+            // \File::move($this->seleniumPath.$this->getLastFile(), $this->downloadedPath.$this->getLastFile());
 
-            // $driver->takeScreenshot(Storage::path('img/'.(new \DateTime('now'))->format('Y-m-d|H:i:s.u').'-DownloadAndSaveFile.png'));
+            Storage::move($this->seleniumPath.$this->getLastFile(), "{$this->downloadedPath}Teste TKS}");
 
             Log::info('DownloadAndSaveFile.init - RPA executado com sucesso!');
 
@@ -40,6 +45,22 @@ class DownloadAndSaveFileController extends Controller
             Log::error("DownloadAndSaveFile.init - {$e->getMessage()}");
         }
 
-        // $driver->quit();
+        $driver->quit();
+    }
+
+    private function getLastFile()
+    {
+        $timestamp = null;
+        $lastFile = null;
+
+        $directory = new \DirectoryIterator($this->seleniumPath);
+        foreach ($directory as $info) {
+            if (!$info->isDot() && $info->getMTime() > $timestamp) {
+                $lastFile = $info->getFilename();
+                $timestamp = $info->getMTime();
+            }
+        }
+
+        return $lastFile;
     }
 }
