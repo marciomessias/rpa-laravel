90c2eab6ee0906a19c2cce6d485b137a2d3b200f
Marcio Messias <marciomessias.developer@gmail.com>
2023-02-02 16:03:47 -0300

    validado se array possui indices

--- app/Models/StringHandle.php
+++ app/Models/StringHandle.php
@@ -38,26 +38,29 @@ class StringHandle
     protected function getFilteredStringByCoordinates($start = false, $end = false, $indice = 0)
     {
         preg_match_all('/\(.*\) Tj/', $this->getStringByCoordinates($start, $end), $extractRows);
-        $rows = $start === false ? array_reverse($extractRows[0]) : $extractRows[0];
-        return trim(preg_replace('/\(|\) Tj/', '', $rows[$indice]));
+
+        if (count($extractRows[0]) > 0) {
+            $rows = $start === false ? array_reverse($extractRows[0]) : $extractRows[0];
+            return trim(preg_replace('/\(|\) Tj/', '', $rows[$indice]));
+        }
+
+        return '';
     }
 
     protected function getStringByCoordinates($start = false, $end = false)
     {
-        $substrString = '';
-
         if($start) {
             $strposStart = strpos($this->currentStringPage, $start);
             $substrStart = substr($this->currentStringPage, $strposStart + strlen($start), 500);
             $strposEnd = $end === false ? 500 : strpos($substrStart, $end);
-            $substrString = substr($substrStart, 0, $strposEnd);
+            return substr($substrStart, 0, $strposEnd);
         }
 
         if(!$start && $end) {
             $strposEnd = strpos($this->currentStringPage, $end);
-            $substrString = substr($this->currentStringPage, 0, $strposEnd);
+            return substr($this->currentStringPage, 0, $strposEnd);
         }
 
-        return $substrString;
+        return '';
     }
 }
