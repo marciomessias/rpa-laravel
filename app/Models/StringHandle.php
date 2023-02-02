f45c41244be2832715ef90953f6a143e19606037
Marcio Messias <marciomessias.developer@gmail.com>
2023-02-02 14:25:22 -0300

    remocao funcao antiga nao mais utilizada

--- app/Models/StringHandle.php
+++ app/Models/StringHandle.php
@@ -35,27 +35,6 @@ class StringHandle
         $this->currentStringPage = $currentStringPage;
     }
 
-    protected function getStringPartFromCoordinates($start = false, $end = false, $indice = 1)
-    {
-        if($start) {
-            $strposStart = strpos($this->currentStringPage, $start);
-            $substrStart = substr($this->currentStringPage, $strposStart + strlen($start), 500);
-            $strposEnd = $end === false ? 500 : strpos($substrStart, $end);
-            $substrString = substr($substrStart, 0, $strposEnd);
-            $array = explode(PHP_EOL, $substrString);
-            return trim(substr($array[$indice], 1, strlen($array[1]) - 6)); 
-        }
-
-        if($end) {
-            $strposEnd = strpos($this->currentStringPage, $end);
-            $substrString = substr($this->currentStringPage, 0, $strposEnd);
-            $array = array_reverse(explode(PHP_EOL, $substrString));
-            return trim(substr($array[$indice], 1, strlen($array[$indice]) - 6));
-        }
-
-        return '';
-    }
-
     protected function getFilteredStringByCoordinates($start = false, $end = false, $indice = 0)
     {
         preg_match_all('/\(.*\) Tj/', $this->getStringByCoordinates($start, $end), $extractRows);
@@ -68,15 +47,15 @@ class StringHandle
         $substrString = '';
 
         if($start) {
-            $strposStart = strpos($this->currentTextPage, $start);
-            $substrStart = substr($this->currentTextPage, $strposStart + strlen($start), 500);
+            $strposStart = strpos($this->currentStringPage, $start);
+            $substrStart = substr($this->currentStringPage, $strposStart + strlen($start), 500);
             $strposEnd = $end === false ? 500 : strpos($substrStart, $end);
             $substrString = substr($substrStart, 0, $strposEnd);
         }
 
         if(!$start && $end) {
-            $strposEnd = strpos($this->currentTextPage, $end);
-            $substrString = substr($this->currentTextPage, 0, $strposEnd);
+            $strposEnd = strpos($this->currentStringPage, $end);
+            $substrString = substr($this->currentStringPage, 0, $strposEnd);
         }
 
         return $substrString;
